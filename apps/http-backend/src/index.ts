bb2626f0b302c442f0f891c3d0ffd94b270cd9b6
kartik <kartik200421@gmail.com>
2025-07-14 23:05:03 +0530

    part 9 done

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,1 +1,14 @@
-console.log("object");
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.json({ message: "wsapp" });
+});
+
+app.listen(3000, () => {
+  console.log("Server is running wild");
+});
