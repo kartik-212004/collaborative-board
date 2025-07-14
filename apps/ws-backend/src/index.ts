987c8dc2011bd1fb0b45af573ae92c037ceed378
kartik <kartik200421@gmail.com>
2025-07-14 20:27:53 +0530

    added prettier

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,1 +1,18 @@
-console.log("object");
+import { WebSocket, WebSocketServer } from "ws";
+
+const rooms: Record<string, WebSocket[]> = {};
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on("connection", (ws: WebSocket) => {
+  console.log("Client connected");
+
+  ws.on("message", (data) => {
+    const message = data.toString();
+    console.log("Received:", message);
+
+    ws.send("hello " + message);
+  });
+  ws.on("close", () => {
+    console.log("Client disconnected");
+  });
+});
