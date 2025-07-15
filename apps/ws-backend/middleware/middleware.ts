2897e72a25dd6d4d8f5e607e11e229e46ebb01c7
kartik <kartik200421@gmail.com>
2025-07-15 17:22:18 +0530

    added middleware in ws

--- apps/ws-backend/middleware/middleware.ts
+++ apps/ws-backend/middleware/middleware.ts
@@ -1,21 +1,17 @@
-import express, { NextFunction, Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 import jwt, { JwtPayload } from "jsonwebtoken";
 
-export async function middleware(
+export default async function middleware(
   req: Request,
   res: Response,
   next: NextFunction,
 ) {
   try {
-    const token = req.headers["authorization"]?.split(" ")[1];
-    // const token = req.headers["authorization"];
-
-    if (!token) {
-      return res.status(401).json({ message: "No token found" });
-    }
+    const token = req.headers.authorization?.split(" ")[1];
+    if (!token) return res.json({ message: "No Token Provided" });
     const decoded = jwt.verify(token, process.env.SECRET_KEY!) as JwtPayload;
-
     req.user = decoded;
+    next();
   } catch (error: any) {
     return res.status(403).json({
       message: "Invalid or expired token",
--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -3,7 +3,12 @@ import { WebSocket, WebSocketServer } from "ws";
 const rooms: Record<string, WebSocket[]> = {};
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on("connection", (ws: WebSocket) => {
+wss.on("connection", (ws: WebSocket, request) => {
+  const url = request.url;
+  if (!url) return;
+
+  const queryParams = new URLSearchParams(url.split("?")[1]);
+  const token = queryParams.get("token");
   console.log("Client connected");
 
   ws.on("message", (data) => {
