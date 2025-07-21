0dd854e9a3097ebb5b1d0a10170453a9a70cc18f
kartik <kartik200421@gmail.com>
2025-07-21 15:15:01 +0530

    DRAWING LOGIC DONE

--- apps/web/app/rooms/[roomId]/page.tsx
+++ apps/web/app/rooms/[roomId]/page.tsx
@@ -5,106 +5,84 @@ import React, { useEffect, useRef, use, useState } from "react";
 import Toolbar from "@/components/Toolbar";
 
 export default function Drawing({ params }: { params: Promise<{ roomId: string }> }) {
-  let Xin: number, Yin: number, Xout: number, Yout: number;
-  const { roomId } = use(params);
+  const [select, setSelect] = useState<"rectangle" | "circle">("circle");
   const canvasRef = useRef<HTMLCanvasElement>(null);
-  const [element, setelement] = useState<"rectangle" | "circle">("circle");
-  const Square: { Xin: number; Yin: number; Xout: number; Yout: number }[] = [];
-  const Circle: { Xin: number; Yin: number; radius: number; startAngle: number; endAngle: number }[] = [];
+  const ctxRef = useRef<CanvasRenderingContext2D | null>(null);
+  let Square: { Yin: number; Xin: number; Xout: number; Yout: number }[] = [];
+  let Circle: { Yin: number; Xin: number; radius: number; startAngle: number; endAngle: number }[] = [];
+  let radius = 0;
+
+  let Xin: number, Yin: number, Xout: number, Yout: number;
+
+  let clicked = false;
+
+  function handleMouseDown(e: MouseEvent) {
+    clicked = true;
+    Xin = e.clientX;
+    Yin = e.clientY;
+    console.log(Xin, Yin);
+  }
+
+  function handleMouseUp(e: MouseEvent) {
+    clicked = false;
+    Xout = e.clientX - Xin;
+    Yout = e.clientY - Yin;
+    console.log(Xout, Yout);
+    Square.push({ Xin, Yin, Xout, Yout });
+    Circle.push({ Xin, Yin, radius, startAngle: 0, endAngle: 2 * Math.PI });
+  }
+
+  function handleMouseMove(e: MouseEvent) {
+    if (clicked) {
+      Xout = e.clientX - Xin;
+      Yout = e.clientY - Yin;
+      console.log(Xout, Yout);
+      if (select == "rectangle") {
+        if (!ctxRef.current) return;
+
+        ctxRef.current.clearRect(0, 0, canvasRef.current!.width, canvasRef.current!.height);
+        ctxRef.current.strokeRect(Xin, Yin, Xout, Yout);
+        Square.map((e) => {
+          if (!ctxRef.current) return;
+          ctxRef.current.strokeRect(e.Xin, e.Yin, e.Xout, e.Yout);
+        });
+      }
+      if (select == "circle") {
+        if (!ctxRef.current || !canvasRef.current) return;
+
+        ctxRef.current.beginPath();
+
+        radius = Math.sqrt(Math.pow(Xout, 2) + Math.pow(Yout, 2));
+        ctxRef.current.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
+        ctxRef.current.arc(Xin, Yin, radius, 0, 2 * Math.PI);
+        ctxRef.current.stroke();
+        Circle.map((e) => {
+          console.log(Circle);
+          if (!ctxRef.current || !canvasRef.current) return;
+          ctxRef.current.beginPath();
+          ctxRef.current.arc(e.Xin, e.Yin, e.radius, 0, 2 * Math.PI);
+          ctxRef.current.stroke();
+        });
+      }
+    }
+  }
 
   useEffect(() => {
-    if (canvasRef.current) {
-      const canvas = canvasRef.current;
-      const ctx = canvas.getContext("2d");
-
-      let clicked = false;
-      canvas.addEventListener("mousedown", (e) => {
-        clicked = true;
-        Xin = e.clientX;
-        Yin = e.clientY;
-      });
-      canvas.addEventListener("mouseup", (e) => {
-        clicked = false;
-        Xout = e.offsetX - Xin;
-        Yout = e.offsetY - Yin;
-        if (element == "rectangle") {
-          Square.push({ Xin, Yin, Xout, Yout });
-        } else if (element == "circle") {
-          const rect = canvas.getBoundingClientRect();
-          const mouseX = e.clientX - rect.left;
-          const mouseY = e.clientY - rect.top;
-
-          const dx = mouseX - Xin;
-          const dy = mouseY - Yin;
-          const radius = Math.sqrt(dx * dx + dy * dy);
-
-          Circle.push({
-            Xin,
-            Yin,
-            radius,
-            startAngle: 0,
-            endAngle: 2 * Math.PI,
-          });
-        }
-      });
-      canvas.addEventListener("mousemove", (e) => {
-        if (clicked) {
-          if (element == "rectangle") {
-            const Xout = e.clientX - Xin;
-            const Yout = e.clientY - Yin;
-            if (!ctx) return;
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
-            ctx.strokeStyle = "black";
-            ctx.strokeRect(Xin, Yin, Xout, Yout);
-            Square.map((e) => {
-              ctx.strokeRect(e.Xin, e.Yin, e.Xout, e.Yout);
-            });
-
-            Circle.map((e) => {
-              ctx.beginPath();
-              ctx.arc(e.Xin, e.Yin, e.radius, 0, 2 * Math.PI);
-
-              ctx.strokeStyle = "black";
-              ctx.stroke();
-            });
-          }
-          if (element === "circle") {
-            const rect = canvas.getBoundingClientRect();
-            const mouseX = e.clientX - rect.left;
-            const mouseY = e.clientY - rect.top;
-
-            const dx = mouseX - Xin;
-            const dy = mouseY - Yin;
-
-            const radius = Math.sqrt(dx * dx + dy * dy);
-
-            if (!ctx) return;
-
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-            Square.map((e) => {
-              ctx.strokeRect(e.Xin, e.Yin, e.Xout, e.Yout);
-            });
-
-            Circle.map((e) => {
-              ctx.beginPath();
-              ctx.arc(e.Xin, e.Yin, e.radius, 0, 2 * Math.PI);
-              ctx.stroke();
-            });
-
-            ctx.beginPath();
-            ctx.arc(Xin, Yin, radius, 0, 2 * Math.PI);
-            ctx.strokeStyle = "black";
-            ctx.stroke();
-          }
-        }
-      });
-    }
-  }, []);
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+    ctxRef.current = canvas.getContext("2d");
+    canvas?.addEventListener("mousedown", handleMouseDown);
+    canvas?.addEventListener("mouseup", handleMouseUp);
+    canvas?.addEventListener("mousemove", handleMouseMove);
+  });
   return (
     <>
-      <Toolbar element={element} setelement={setelement} />
-      <canvas ref={canvasRef} width={1920} height={925} className="bg-gray-200 text-black"></canvas>;
+      <div></div>
+      <canvas
+        ref={canvasRef}
+        height={window.innerHeight}
+        width={window.innerWidth}
+        className="bg-gray-100"></canvas>
     </>
   );
 }
