135131a67b9c2a822bc030012871f16ab4e63ca4
Kartik <kartik200421@gmail.com>
2026-01-18 16:17:32 +0530

    lazycommmit, please ignore

--- packages/prisma/src/index.ts
+++ packages/prisma/src/index.ts
@@ -1,10 +1,10 @@
-import { PrismaClient } from "@prisma/client";
-import dotenv from "dotenv";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@prisma/client/extension";
+import "dotenv/config";
 
-dotenv.config();
+const connectionString = `${process.env.DATABASE_URL}`;
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter });
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+export { prisma };
